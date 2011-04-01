f09937854ead18c2ba2b6e21a119948cfc1303cd
Jeff Welling <jeff.welling@gmail.com>
2011-04-01 19:10:33 -0400

    Started writing sync mod for github issues

---
+++ lib/ticgit-ng/sync/github_issues.rb
@@ -0,0 +1,16 @@
+module TicGitNG
+  module Sync
+    module Github_Issues
+      def index
+      end
+      def show
+      end
+      def create
+      end
+      def update
+      end
+      def destroy
+      end
+    end
+  end
+end
